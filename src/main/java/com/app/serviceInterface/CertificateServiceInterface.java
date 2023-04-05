6549a9ce1fe5db48f9dca8ca2c1536777e5d561b
surajsonawane <surajsonawane@nimapinfotech.com>
2023-04-05 18:33:22 +0530

    changes

--- src/main/java/com/app/serviceInterface/CertificateServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.app.serviceInterface;
-
-import javax.servlet.http.HttpServletResponse;
-
-public interface CertificateServiceInterface {
-
-	void generatePdf(Long certificateId, HttpServletResponse response) throws Exception;
-
-}
